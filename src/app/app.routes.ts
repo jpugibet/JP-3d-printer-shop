96516070692ecf65df08de23e96830f388f30642
jpugibet <jpugibet1@hotmail.com>
2026-02-10 17:35:50 +0100

    Create app.routes.ts with Angular routes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/catalog', pathMatch: 'full' },
+  { path: 'catalog', loadChildren: () => import('./catalog/catalog.module').then(m => m.CatalogModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
